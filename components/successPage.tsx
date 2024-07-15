9d833a20d798744984e6eb8a0c6b33cc7e80f0e3
Defry Socop <venturadef@gmail.com>
2024-07-14 22:36:21 -0600

    Responsive desing
    Se creo una vista responsiva

--- components/successPage.tsx
+++ components/successPage.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import { FaSeedling } from "react-icons/fa";
-//Para uso de rutas
 import { useRouter } from 'next/navigation';
 
 const SuccessPage = () => {
@@ -13,21 +12,23 @@ const SuccessPage = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8 flex flex-col items-center relative">
-      <img 
-        className="h-24 absolute top-0 left-0 mt-8 ml-4 mb-8"
-        src="https://disagro-s3.s3.us-east-2.amazonaws.com/web-disagro/logotipo-disagro-verde.png"
-        alt="Logotipo Disagro"
-      />
-      <h1 className="text-3xl font-bold text-green-900 mb-8 text-center mt-12">Registro Exitoso</h1>
-      <FaSeedling className="text-green-700 text-6xl mb-4" />
-      <p className="text-center text-lg mb-8">Gracias por registrarte. Hemos recibido tu información con éxito.</p>
-      <button
-        onClick={handleReturnHome}
-        className="py-2 px-4 bg-green-800 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 focus:ring-opacity-75"
-      >
-        Regresar
-      </button>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
+        <img 
+          className="h-16 sm:h-24 mx-auto mb-8"
+          src="https://disagro-s3.s3.us-east-2.amazonaws.com/web-disagro/logotipo-disagro-verde.png"
+          alt="Logotipo Disagro"
+        />
+        <h1 className="text-2xl sm:text-3xl font-bold text-green-900 text-center mb-4">Registro Exitoso</h1>
+        <FaSeedling className="text-green-700 text-4xl sm:text-6xl mb-4 mx-auto" />
+        <p className="text-center text-base sm:text-lg mb-8">Gracias por registrarte. Hemos recibido tu información con éxito.</p>
+        <button
+          onClick={handleReturnHome}
+          className="w-full py-2 px-4 bg-green-800 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 focus:ring-opacity-75"
+        >
+          Regresar
+        </button>
+      </div>
     </div>
   );
 };
